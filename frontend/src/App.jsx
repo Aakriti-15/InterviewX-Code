fedb2c5edaa88353108d2db6840405ece0da93bd
Aakriti <aakritia30@gmail.com>
2026-02-14 17:17:01 +0530

    frontend tools setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,25 @@
 import { SignInButton, SignOutButton, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
-import './App.css'
-
+import { Routes, Route } from 'react-router';
+import HomePage from './pages/HomePage';
+import ProblemsPage from './pages/ProblemsPage';
+import { useUser } from '@clerk/clerk-react';
+import { Navigate } from 'react-router';
+import { Toaster } from 'react-hot-toast';
 function App() {
   
-
+ const {isSignedIn} = useUser();
   return (
-    <>
-      <h1>Welcome to the app</h1>
-      <SignedOut>
-       <SignInButton mode ="modal">
-        <button>
-         Login
-        </button>
-       </SignInButton>
-      </SignedOut>
+     <>
+      <Routes>
       
-      <SignedIn>
-         <SignOutButton />
-      </SignedIn>
-      <UserButton />
+      <Route path ="/" element ={<HomePage />} />
+      <Route path ="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"}/>} />
+
+       </Routes>
+       <Toaster position='top-left' toastOptions={{duration:3000}} />
        </>
-  )
+  );
 }
 
-export default App
+export default App;
+//tw , daisyui, react-router, react-hot-toast,
