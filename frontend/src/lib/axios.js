e4e9eb4049a03ccf067f73478009e282d999a282
Aakriti <aakritia30@gmail.com>
2026-02-24 03:12:21 +0530

    hardcode baseURL for debugging

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    baseURL: window.location.hostname !== 'localhost'
-    ? "https://interviewx-code-backend.onrender.com/api"
-    : "http://localhost:3000/api",
+    baseURL: "https://interviewx-code-backend.onrender.com/api",
     withCredentials:true,
 });
 
