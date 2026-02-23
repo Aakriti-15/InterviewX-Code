4a696a2549bdbc0c183c9c40253ba0dedfa96498
Aakriti <aakritia30@gmail.com>
2026-02-24 03:07:08 +0530

    fixed errors in axios.js 2

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const isProd = window.location.hostname !== 'localhost';
 
 const axiosInstance = axios.create({
-    baseURL: isProd
+    baseURL: window.location.hostname !== 'localhost'
     ? "https://interviewx-code-backend.onrender.com/api"
     : "http://localhost:3000/api",
     withCredentials:true,
