df0e8e8bd35569b5646a77f13712f140a334d53d
Aakriti <aakritia30@gmail.com>
2026-02-24 02:33:58 +0530

    fixed errors 1

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
     withCredentials:true, // by adding this field browser will send cookies with every request to server automatically on every single request
 });
 
